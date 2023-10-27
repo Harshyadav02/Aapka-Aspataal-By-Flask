41c24c35ae665a3e2aec58113b86ccef9a5dfdc4
harshyadav02 <hy043768@gmail.com>
2023-10-27 16:34:44 +0530

    models

--- models.py
+++ models.py
@@ -11,3 +11,23 @@ class Admin(Base,UserMixin):
     username = Column(String(20), primary_key=True)
     password = Column(String(255))
 
+class Nurse(Base , UserMixin):
+    __tablename__ = 'Nurse'
+
+    username = Column(String(20) , primary_key=True)
+    password = Column(String(255) , nullable=False)
+    First_Name  = Column(String(20) , nullable=False)
+    Last_Name = Column(String(20) , nullable=False)
+    contact = Column(Integer , nullable=False)
+    role = Column(String(20) , default='Nurse' , nullable=False)
+
+class Chemist(Base , UserMixin):
+    __tablename__ = 'Chemist'
+
+    username = Column(String(20) , primary_key=True)
+    password = Column(String(255) , nullable=False , default=None)
+    First_Name  = Column(String(20) , nullable=False)
+    Last_Name = Column(String(20) , nullable=False)
+    contact = Column(Integer , nullable=False)
+    role = Column(String(20) , default='Chemist' , nullable=False)
+
